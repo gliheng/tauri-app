ea13529df0796c8f90c6aca9e0d0ec533a3fd618
juju <gliheng@gmail.com>
2025-07-30 23:38:41 +0800

    feat: change logo

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -77,7 +77,7 @@ function openNote(note: Note) {
     class="flex-1 size-full bg-elevated flex flex-col p-2 gap-2 items-stretch"
   >
     <h1 class="text-xl font-bold mb-2 flex flex-row gap-2 items-center">
-      <img class="w-16 h-16" src="/logo.png" alt="Logo" />
+      <img class="w-16 h-16" src="/raven.png" alt="Logo" />
       Raven
     </h1>
     <UButton
